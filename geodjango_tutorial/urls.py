8a2569cb1cc2bd17e2df787b89c1c9ccd39f43a2
emilroy4 <emilroy100@gmail.com>
2024-12-11 13:32:12 +0000

    update settings.py for static

--- geodjango_tutorial/urls.py
+++ geodjango_tutorial/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import include, path
 
  
@@ -26,4 +27,4 @@ urlpatterns = [
 	path('', include('world.urls')),
 	path('events/', include('events.urls')),
 
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
