d2fc730586c4fb6b5aa44d612bd2931c11592093
emilroy4 <emilroy100@gmail.com>
2024-12-11 13:15:48 +0000

    update for logo

--- geodjango_tutorial/settings.py
+++ geodjango_tutorial/settings.py
@@ -132,9 +132,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),  # Custom static files directory
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
