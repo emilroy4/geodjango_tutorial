ebf798ec8eb851809caa6d4fdc429a55ced83fb9
emilroy4 <emilroy100@gmail.com>
2024-12-17 17:36:19 +0000

    without images

--- events/management/commands/fetch_event_images.py
+++ events/management/commands/fetch_event_images.py
@@ -3,15 +3,15 @@ from events.models import Event
 from events.utils.importimages import fetch_image_from_google
 
 class Command(BaseCommand):
-    help = 'Fetch and replace images for all events from Google'
+    help = 'Fetch images for events from Google'
 
     def handle(self, *args, **kwargs):
-        events = Event.objects.all()  # Fetch all events, regardless of current image status
+        events = Event.objects.filter(image_url__isnull=True)  # Only fetch for events without images
         for event in events:
             image_url = fetch_image_from_google(event.title)
             if image_url:
                 event.image_url = image_url
                 event.save()
-                self.stdout.write(self.style.SUCCESS(f'Replaced image for {event.title}'))
+                self.stdout.write(self.style.SUCCESS(f'Updated image for {event.title}'))
             else:
                 self.stdout.write(self.style.WARNING(f'No image found for {event.title}'))
