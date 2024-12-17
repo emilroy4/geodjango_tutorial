970ef5923a482fee0b85d6317be2a6968b7e6ebb
emilroy4 <emilroy100@gmail.com>
2024-12-17 17:56:36 +0000

    fixed env

--- events/utils/importimages.py
+++ events/utils/importimages.py
@@ -6,15 +6,18 @@ def fetch_image_from_google(query):
     search_engine_id = settings.GOOGLE_SEARCH_ENGINE_ID
     search_url = "https://www.googleapis.com/customsearch/v1"
 
-    # Exclude images from Facebook using -site:facebook.com
+    if not api_key or not search_engine_id:
+        print("Error: GOOGLE_API_KEY or GOOGLE_SEARCH_ENGINE_ID is not set.")
+        return None
+
     query_with_exclusions = f"{query} -site:facebook.com"
 
     params = {
-        "q": query_with_exclusions,  # Query with exclusions
+        "q": query_with_exclusions,
         "cx": search_engine_id,
         "key": api_key,
         "searchType": "image",
-        "num": 1  # Number of results to return
+        "num": 1
     }
 
     try:
@@ -22,11 +25,8 @@ def fetch_image_from_google(query):
         response.raise_for_status()
         search_results = response.json()
 
-        # Get the first image URL from the results
         if 'items' in search_results:
             return search_results['items'][0]['link']
-        else:
-            print(f"No images found for query: {query}")
     except requests.RequestException as e:
         print(f"Error fetching image: {e}")
     return None
--- geodjango_tutorial/settings.py
+++ geodjango_tutorial/settings.py
@@ -12,6 +12,14 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
+GOOGLE_SEARCH_ENGINE_ID = os.getenv('GOOGLE_SEARCH_ENGINE_ID')
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
